8c157b53365fb3f7ff22f4f62e77b6902b68ad65
MohamedAliSamk <MohamedAliSmk@gmail.com>
2023-04-12 01:30:34 +0200

    pages solved !!

--- apps/home/views.py
+++ apps/home/views.py
@@ -19,28 +19,6 @@ def get_news_data(stock):
     data = json.loads(response.text)
     return data
 
-def CompanyDetails(Ticker):
-    # Define the API endpoint and parameters
-    apikey = "29de022d931f42e73af5b5884f4e970d"
-    url = f"https://financialmodelingprep.com/api/v3/profile/{Ticker}?apikey={apikey}"
-    # Send a GET request to the API and parse the JSON response
-    response = requests.get(url)
-    company_data = response.json()
-    if len(company_data) > 0:
-        return company_data[0]
-    else:
-        return print("there is no values")
-    
-    
-
-tickers = ["TSLA", "APPL", "FB", "GOOG" , "MSFT" ,"SBUX","MBG.DE","2222.SR", "CIB" , "QNBK" ,"ETEL" ,"EGS3G0Z1C014.CA" ,"EGS3C251C013.CA"]
-data_flow = {}
-   
-for ticker in tickers:
-    data = CompanyDetails(ticker)
-    data_flow.update({ticker: {"image": data["image"], "price": data["price"], "changes": data["changes"]}})
-
-print(data_flow)  
 #@login_required(login_url="/login/")
 def index(request):   
     language = request.GET.get("lang", "en") # retrieve language preference from GET parameter or default to "en"
--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 import requests
-url = "https://financialmodelingprep.com/api/v3/stock_news?tickers=AAPL,FB,GOOG,AMZN&page=0&apikey=29de022d931f42e73af5b5884f4e970d"
-params = {
-        "tickers": "AAPL,FB,GOOG,AMZN",
-        "page": "0",
-        "apikey": "29de022d931f42e73af5b5884f4e970d"
-    }
 
+def CompanyDetails(Ticker):
+    # Define the API endpoint and parameters
+    apikey = "29de022d931f42e73af5b5884f4e970d"
+    url = f"https://financialmodelingprep.com/api/v3/profile/{Ticker}?apikey={apikey}"
     # Send a GET request to the API and parse the JSON response
-response = requests.get(url, params=params)
-data = response.json()
-print(data)
+    response = requests.get(url)
+    company_data = response.json()
+    if len(company_data) > 0:
+        return company_data[0]
+    else:
+        return print("there is no values")
+    
+
+tickers = ["TSLA", "APPL", "FB", "GOOG" , "MSFT" ,"SBUX","MBG.DE","2222.SR", "CIB" , "QNBK" ,"ETEL" ,"EGS3G0Z1C014.CA" ,"EGS3C251C013.CA"]
+data_flow = {}   
+for ticker in tickers:
+    data = CompanyDetails(ticker)
+    data_flow.update({ticker: {"image": data["image"], "price": data["price"], "changes": data["changes"] ,"company Name": data["companyName"]}})
+
+print(data_flow)  
